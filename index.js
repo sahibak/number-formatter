156f9f9ce2192b4121a7abd077c1564a415bc2e2
Sahiba K <khanna.sahiba@gmail.com>
2020-03-29 16:26:20 -0400

    stack built

--- index.js
+++ index.js
@@ -1,11 +1,45 @@
 'use strict';
 
 /**
- * Adds commas to a number
- * @param {number} number
- * @param {string} locale
+ * Push and pop from a stack
+ * @param {string} value
+ * @param {number} value
  * @return {string}
+ * @return {array}
  */
-module.exports = function(number, locale) {
-    return number.toLocaleString(locale);
-};
+
+//  creating a stack of length 5
+let lengthOfStack = 5
+let stack = new Array(lengthOfStack);
+let lastUpdatedIndex = 0;
+
+addValueToStack = (value) => {
+// pushing value onto the stack
+    if (lastUpdatedIndex === lengthOfStack){
+        return "Stack is full, cannot push."
+    }
+    else {
+        stack[lastUpdatedIndex] = value;
+        lastUpdatedIndex += 1;
+        return stack;
+    }
+}
+
+removeValueFromStack = (value) => {
+    // popping values from a stack
+    if (lastUpdatedIndex === 0){
+        return "Stack is empty,nothing to pop."
+    }
+    else {
+        stack.splice(1,lastUpdatedIndex - 1);
+        lastUpdatedIndex -= 1;
+        return stack;
+    }
+}
+
+printStack = () => {
+    // printing the stack
+    return stack;
+}
+
+
