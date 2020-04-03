5a2600c32cf9cd16d5136b97af6d2f6cd909a814
Sahiba K <khanna.sahiba@gmail.com>
2020-04-03 13:29:15 -0400

    added prettier, added input/output values to class, adjusted the check of the length input, output in selected fucntion revised to {stack}, adjusted comment type from // to /* for stack file.

--- library/stack.js
+++ library/stack.js
@@ -1,72 +1,77 @@
-'use strict';
+"use strict";
 
 class Stack {
-    //  creating a stack of length = lengthOfStack
-    // creating a new array of the length provided
-    // setting up initial index to 0
-    constructor(lengthOfStack) {
-        this.lengthOfStack = this.verifyLengthInput(lengthOfStack);
-        this.position = 0;
-        this.stack = new Array(this.lengthOfStack);
-    }
+  /**
+   * creating a stack of length = lengthOfStack
+   * creating a new array of the length provided
+   * setting up initial index to 0
+   * @param {integer} lengthOfStack
+   * @return {stack}
+   */
+  constructor(lengthOfStack) {
+    this.verifyLengthInput(lengthOfStack);
+    this.lengthOfStack = lengthOfStack;
+    this.position = 0;
+    this.stack = new Array(this.lengthOfStack);
+  }
 
-    verifyLengthInput(lengthOfStack) {
-    // if the lengthOfStack provided in stack is non-numeric, setting lengthOfStak === 0
-    /** 
-    * @param {integer} lengthOfStack
-    * @return {integer}
-    */
-        if (!(Number.isInteger(lengthOfStack))) {
-            throw new Error("Provide integer value for length.")
-        }
-        return lengthOfStack;
+  verifyLengthInput(lengthOfStack) {
+    /**
+     * if the lengthOfStack provided in stack is non-numeric, setting lengthOfStak === 0
+     * @param {integer} lengthOfStack
+     * @return {integer}
+     */
+    if (!Number.isInteger(lengthOfStack)) {
+      throw new Error("Provide integer value for length.");
     }
+    return lengthOfStack;
+  }
 
-    push(value) {
-    // pushing value onto the stack
-    // if stack is full, throwing an exception
-    /** 
-    * @param {any} value
-    * @return {array}
-    */
-        if (this.position === this.lengthOfStack) {
-            throw new Error ("Error");
-        }
-        this.stack[this.position] = value;
-        this.position += 1;
-        return this.stack;
+  push(value) {
+    /**
+     * pushing value onto the stack
+     * if stack is full, throwing an exception
+     * @param {any} value
+     * @return {stack}
+     */
+    if (this.position === this.lengthOfStack) {
+      throw new Error("Error");
     }
+    this.stack[this.position] = value;
+    this.position += 1;
+    return this.stack;
+  }
 
-    pop() {
-    // popping values from a stack
-    // if stack is empty, throwing an exception
-    /** 
-    * @return {any}
-    */
-        if(this.position === 0) {
-            throw new Error ("Error");
-        }
-        this.position -= 1;
-        let poppedValue = this.stack[this.position];
-        this.stack[this.position] = null;
-        return poppedValue;
+  pop() {
+    /**
+     * popping values from a stack
+     * if stack is empty, throwing an exception
+     * @return {any}
+     */
+    if (this.position === 0) {
+      throw new Error("Error");
     }
+    this.position -= 1;
+    let poppedValue = this.stack[this.position];
+    this.stack[this.position] = null;
+    return poppedValue;
+  }
 
-    print() {
-        // printing the stack
-        /** 
-        * @return {array}
-        */
-        return this.stack;
-    }
+  print() {
+    /**
+     * printing the stack
+     * @return {stack}
+     */
+    return this.stack;
+  }
 
-    size() {
-        // checking length of stack
-        /** 
-        * @return {integer}
-        */
-        return this.stack.length;
-    }
+  size() {
+    /**
+     * checking length of stack
+     * @return {integer}
+     */
+    return this.stack.length;
+  }
 }
 
-module.exports = {Stack};
+module.exports = { Stack };
