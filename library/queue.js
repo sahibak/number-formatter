c75bec67d6f61b22095779b5628a67bf73583e0e
Sahiba K <khanna.sahiba@gmail.com>
2020-04-05 14:04:55 -0400

    adding prettier; removing count check from push

--- library/queue.js
+++ library/queue.js
@@ -1,109 +1,109 @@
-'use strict';
+"use strict";
 
 class Queue {
-    //  creating a queue of length = lengthOfQueue
-    // creating a new array of the length provided
-    // setting up head and tail values. Head value will determine location of pop and tail value will determine location of push. 
-    // countOfItems keeps a count of number of items in the array at a given time.
-    constructor(lengthOfQueue) {
-        this.lengthOfQueue = this.verifyLengthInput(lengthOfQueue);
-        this.head = 0;
-        this.tail = 0;
-        this.countOfItems = 0;
-        this.queue = new Array(this.lengthOfQueue);
-    }
+  //  creating a queue of length = lengthOfQueue
+  // creating a new array of the length provided
+  // setting up head and tail values. Head value will determine location of pop and tail value will determine location of push.
+  // count keeps a count of number of items in the array at a given time.
+  constructor(lengthOfQueue) {
+    this.lengthOfQueue = this.verifyLengthInput(lengthOfQueue);
+    this.head = 0;
+    this.tail = 0;
+    this.count = 0;
+    this.queue = new Array(this.lengthOfQueue);
+  }
 
-    verifyLengthInput(lengthOfQueue) {
-        /** 
-        * Verify the lengthOfQueue provided is an integer.
-        * @param {integer} lengthOfQueue
-        * @return {integer}
-        */
-        if (!(Number.isInteger(lengthOfQueue))) {
-            throw new Error("Only accept integer as length");
-        }
-        return lengthOfQueue;
+  verifyLengthInput(lengthOfQueue) {
+    /**
+     * Verify the lengthOfQueue provided is an integer.
+     * @param {integer} lengthOfQueue
+     * @return {integer}
+     */
+    if (!Number.isInteger(lengthOfQueue)) {
+      throw new Error("Only accept integer as length");
     }
+    return lengthOfQueue;
+  }
 
-    push(value) {
-        /** 
-        * Pushing value onto the queue
-        * If queue is full and head===tail, an error is thrown as no more additions to the queue is allowed. 
-        * The tail value is reset to zero once it equals lengthOfQueue.
-        * @param {any} value
-        * @return {array}
-        */
-        if (this.tail === this.head && this.countOfItems === this.lengthOfQueue) {
-            throw new Error ("Error");
-        }
-        this.queue[this.tail] = value;
-        this.countOfItems += 1;
-        this.tail += 1;
-        this.tail === this.lengthOfQueue ? this.tail = this.tail % this.lengthOfQueue : this.tail;
-        return this.queue;
+  push(value) {
+    /**
+     * Pushing value onto the queue
+     * @param {any} value
+     * @return {array}
+     */
+    if (this.tail === this.head) {
+      // && this.count === this.lengthOfQueue
+      throw new Error("Error");
     }
+    this.queue[this.tail] = value;
+    this.count += 1;
+    this.tail += 1;
+    this.tail === this.lengthOfQueue
+      ? (this.tail = this.tail % this.lengthOfQueue)
+      : this.tail;
+    return this.queue;
+  }
 
-    pop() {
-        /** 
-        * Popping values from a queue
-        * If queue is empty, and head === tail an error is thrown as no more removals from the queue are allowed. 
-        * The head value is reset to zero once it equals lengthOfQueue.
-        * @return {any}
-        */
-        if(this.head === this.tail && this.countOfItems === 0) {
-            throw new Error ("Error");
-        }
-        let poppedValue = this.queue[this.head];
-        this.queue[this.head] = null;
-        this.head += 1 ;
-        this.head === this.lengthOfQueue ? this.head = this.head % this.lengthOfQueue : this.head;
-        this.countOfItems -= 1;
-        return poppedValue;
+  pop() {
+    /**
+     * Popping values from a queue
+     * @return {any}
+     */
+    if (this.head === this.tail && this.count === 0) {
+      throw new Error("Error");
     }
+    let poppedValue = this.queue[this.head];
+    this.queue[this.head] = null;
+    this.head += 1;
+    this.head === this.lengthOfQueue
+      ? (this.head = this.head % this.lengthOfQueue)
+      : this.head;
+    this.count -= 1;
+    return poppedValue;
+  }
 
-    print() {
-        /** 
-        * printing the queue
-        * @return {array}
-        */
-        return this.queue;
+  print() {
+    /**
+     * printing the queue
+     * @return {array}
+     */
+    return this.queue;
+  }
+
+  size() {
+    /**
+     * checking length of queue
+     * @return {integer}
+     */
+    return this.queue.length;
+  }
+
+  createQueue(listOfValues) {
+    /**
+     * Add values to the empty queue till it is full, from a list of values provides.
+     * This function is used for testing.
+     * @return {array}
+     */
+    for (let i = 0; i < this.lengthOfQueue; i++) {
+      this.queue[i] = listOfValues[i];
+      this.count += 1;
     }
+    return this.queue;
+  }
 
-    size() {
-        /**
-        * checking length of queue
-        * @return {integer}
-        */
-        return this.queue.length;
+  emptyQueue() {
+    /**
+     * Empty the queue.
+     * This function is used for testing.
+     * @return {array}
+     */
+    for (let i = this.count; i > 0; i--) {
+      this.queue[this.head] = null;
+      this.head += 1;
+      this.head === this.lengthOfQueue ? (this.head = 0) : this.head;
     }
-
-    createQueue(listOfValues) {
-        /**
-        * Add values to the empty queue till it is full, from a list of values provides.
-        * This function is used for testing.
-        * @return {array}
-        */
-        for (let i = 0; i < this.lengthOfQueue; i++) {
-            this.queue[i] = listOfValues[i];
-            this.countOfItems += 1;
-        }
-        return this.queue;
-    }
-
-    emptyQueue() {
-        /**
-        * Empty the queue.
-        * This function is used for testing.
-        * @return {array}
-        */
-        for (let i = this.countOfItems; i > 0; i--) {
-            this.queue[this.head] = null;
-            this.head += 1;
-            this.head === this.lengthOfQueue ? this.head = 0 : this.head;
-        }
-        return this.queue;
-    }
-
+    return this.queue;
+  }
 }
 
-module.exports = {Queue};
+module.exports = { Queue };
--- test/test.js
+++ test/test.js
@@ -3,56 +3,55 @@
 const expect = require("chai").expect;
 const { Queue } = require("../library/queue");
 
-// creating queues for testing 
+// creating queues for testing
 let queue2 = new Queue(3);
 let queue3 = new Queue(0);
 
-describe("#Updating queue2", function() {
-    it("should add values to full length of queue2", function() {
-        let result = queue2.createQueue([1,2,"xyz"]);
-        expect(result).to.eql([1,2,"xyz"]);
-    });
+describe("#Updating queue2", function () {
+  it("should add values to full length of queue2", function () {
+    let result = queue2.createQueue([1, 2, "xyz"]);
+    expect(result).to.eql([1, 2, "xyz"]);
+  });
 
-    it("should confirm the correct value is popped from queue2", function() {
-        let result = queue2.pop();
-        expect(result).to.equal(1);
-    });
+  it("should confirm the correct value is popped from queue2", function () {
+    let result = queue2.pop();
+    expect(result).to.equal(1);
+  });
 
-    it("should confirm queue2 is correct after pop", function() {
-        let result = queue2.print();
-        expect(result).to.eql([null,2,"xyz"]);
-    });
+  it("should confirm queue2 is correct after pop", function () {
+    let result = queue2.print();
+    expect(result).to.eql([null, 2, "xyz"]);
+  });
 
-    it("should push value to the right position in queue2", function() {
-        let result = queue2.push("4");
-        expect(result).to.eql(["4",2,"xyz"]);
-    });
+  it("should push value to the right position in queue2", function () {
+    let result = queue2.push("4");
+    expect(result).to.eql(["4", 2, "xyz"]);
+  });
 
-    it("should pop till queue2 empty", function() {
-        let result = queue2.emptyQueue();
-        expect(result).to.eql([null,null,null]);
-    });
+  it("should pop till queue2 empty", function () {
+    let result = queue2.emptyQueue();
+    expect(result).to.eql([null, null, null]);
+  });
 
-    it("shoudl give an error when trying to pop from empty queue2", function() {
-        expect(queue2.pop).to.throw();
-    })
+  it("shoudl give an error when trying to pop from empty queue2", function () {
+    expect(queue2.pop).to.throw();
+  });
 });
 
-describe("#Remove values from queue with length zero", function() {
-    it("throw an error when trying to pop values from queue3", function() {
-        expect(queue3.pop).to.throw();
-    });
+describe("#Remove values from queue with length zero", function () {
+  it("throw an error when trying to pop values from queue3", function () {
+    expect(queue3.pop).to.throw();
+  });
 });
 
-describe("#Should not allow to add values to queue with length zero.", function() {
-    it("should not allow for value to be added to stack3", function() {
-        expect(() => this.queue3.push("xyx") ).to.throw();
-    });
+describe("#Should not allow to add values to queue with length zero.", function () {
+  it("should not allow for value to be added to stack3", function () {
+    expect(() => this.queue3.push("xyx")).to.throw();
+  });
 });
 
-describe("#Initalize a queue with non-integer length value", function() {
-    it("should throw and error", function() {
-        expect(() => new Queue("n")).to.throw();
-    });
+describe("#Initalize a queue with non-integer length value", function () {
+  it("should throw and error", function () {
+    expect(() => new Queue("n")).to.throw();
+  });
 });
-
