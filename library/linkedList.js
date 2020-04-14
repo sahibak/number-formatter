60caed3d89faf364954b421558c2c5f556333587
Sahiba K <khanna.sahiba@gmail.com>
2020-04-14 09:10:47 -0400

    linkedList starter

---
+++ library/linkedList.js
@@ -0,0 +1,18 @@
+"use strict";
+
+class LinkedList {
+  constructor() {
+    this.data = 0;
+    this.next = null;
+    this.linkedList = (this.data, this.null);
+  }
+
+  addLink() {
+    if (this.data === 0) {
+      this.data += 1;
+      this.next = new LinkedList();
+      console.log(this.linkedList);
+      return this.linkedList;
+    }
+  }
+}
