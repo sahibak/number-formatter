001440b7aa6315847138b057f5a9cb142e19c1d0
Sahiba K <khanna.sahiba@gmail.com>
2020-05-13 15:29:35 -0400

    first stab at rebalancing

--- library/bTree.js
+++ library/bTree.js
@@ -14,6 +14,10 @@ class Node {
     this.data = data;
     this.right = null;
     this.totalNodesinTree = 1;
+    this.leftNodes = [];
+    this.rightNodes = [];
+    this.balancingFactor = 0;
+    // this.dataArray = [data];
   }
 
   addLink(data) {
@@ -26,19 +30,92 @@ class Node {
      * @return {node}
      */
     let nextNode = this;
+    let balancingRequired = false;
     this.totalNodesinTree += 1;
+    // this.dataArray.push(data);
     while (nextNode != null) {
       if (nextNode.data < data && nextNode.right != null) {
+        nextNode.rightNodes.push(data);
+        nextNode.balancingFactor = this.updateBalancingFactor(nextNode);
+        balancingRequired =
+          balancingRequired === false
+            ? this.checkBalancingFactor(nextNode)
+            : true;
         nextNode = nextNode.right;
       } else if (nextNode.data > data && nextNode.left != null) {
+        nextNode.leftNodes.push(data);
+        nextNode.balancingFactor = this.updateBalancingFactor(nextNode);
+        balancingRequired =
+          balancingRequired === false
+            ? this.checkBalancingFactor(nextNode)
+            : true;
         nextNode = nextNode.left;
       } else if (nextNode.data < data && nextNode.right === null) {
+        nextNode.rightNodes.push(data);
+        nextNode.balancingFactor = this.updateBalancingFactor(nextNode);
+        balancingRequired =
+          balancingRequired === false
+            ? this.checkBalancingFactor(nextNode)
+            : true;
         nextNode.right = new Node(data);
         break;
       } else if (nextNode.data > data && nextNode.left === null) {
+        nextNode.leftNodes.push(data);
+        nextNode.balancingFactor = this.updateBalancingFactor(nextNode);
+        balancingRequired =
+          balancingRequired === false
+            ? this.checkBalancingFactor(nextNode)
+            : true;
         nextNode.left = new Node(data);
         break;
       }
+    }
+    balancingRequired === true ? this.rebalanceTree() : null;
+    return this;
+  }
+
+  updateBalancingFactor(nextNode) {
+    let leftHeight =
+      nextNode.leftNodes.length % 2 === 0 || nextNode.leftNodes.length === 1
+        ? nextNode.leftNodes.length
+        : nextNode.leftNodes.length - 1;
+    let rightHeight =
+      nextNode.rightNodes.length % 2 === 0 || nextNode.rightNodes.length === 1
+        ? nextNode.rightNodes.length
+        : nextNode.rightNodes.length - 1;
+    let balancingFactor = leftHeight - rightHeight;
+    return balancingFactor;
+  }
+
+  checkBalancingFactor(node) {
+    if (
+      node.balancingFactor === 0 ||
+      node.balancingFactor === -1 ||
+      node.balancingFactor === 1
+    ) {
+      return false;
+    }
+    return true;
+  }
+
+  rebalanceTree() {
+    console.log("(((((((((((((((((((((((((((((((((((((");
+    let dataArray = [...this.leftNodes, this.data, ...this.rightNodes];
+    let rootDataIndex = Math.floor((dataArray.length - 1) / 2);
+    this.data = dataArray[rootDataIndex];
+    this.left = null;
+    this.right = null;
+    this.totalNodesinTree = 1;
+    this.leftNodes = [];
+    this.rightNodes = [];
+    this.balancingFactor = 0;
+    for (
+      let i = rootDataIndex - 1, n = rootDataIndex + 1;
+      i >= 0 || n < dataArray.length;
+      i -= 1, n += 1
+    ) {
+      !!dataArray[i] ? this.addLink(dataArray[i]) : null;
+      !!dataArray[n] ? this.addLink(dataArray[n]) : null;
     }
     return this;
   }
@@ -47,6 +124,7 @@ class Node {
     let valuesInNodes = [];
     let countOfNodes = this.totalNodesinTree;
     let currentNodes = [this];
+    console.log("--------------------", this);
     while (countOfNodes > 0) {
       let nextNodes = [];
       for (let i = 0; i < currentNodes.length; i++) {
@@ -57,6 +135,7 @@ class Node {
       }
       currentNodes = nextNodes;
     }
+    console.log(valuesInNodes);
     return valuesInNodes;
   }
 
