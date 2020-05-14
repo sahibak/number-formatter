a0392597858ddb221c4a8ebf8aec0cb86c56d079
Sahiba K <khanna.sahiba@gmail.com>
2020-05-14 15:16:58 -0400

    recurssion without classes

--- library/recurssion.js
+++ library/recurssion.js
@@ -1,129 +1,122 @@
 /*link: http://www.bowdoin.edu/~ltoma/teaching/cs107/spring05/recursion.html*/
 
-class PractiseSum {
-  /*
-    creating a class with functions
-    @param {integer} n
-    @return {class} 
-    */
-  constructor(n) {
-    this.n = n;
-  }
-
-  sumOfNNumbers(total = 0, startingNumber = 1) {
-    /** 
+function sumOfNNumbers(number, total = 0, startingNumber = 1) {
+  /** 
       Exercise 1
       Write a recursive function that computes the sum of all numbers from 1 to n, where n is given as parameter.
     
       Function returns the sum of 1 to n numbers
       Assumption: Only positive integers are provided as input
+      @param {integer} number
       @param {integer} total
       @param {integer} startingNumber
       @return {integer}
       */
-    if (startingNumber > this.n) {
-      return total;
-    } else {
-      total = total + startingNumber;
-      startingNumber += 1;
-      return this.sumOfNNumbers(total, startingNumber);
-    }
+  if (startingNumber > number) {
+    return total;
+  } else {
+    total = total + startingNumber;
+    startingNumber += 1;
+    return sumOfNNumbers(number, total, startingNumber);
   }
 }
 
-class PractiseArrays {
+function findMinimum(listOfValues, size, minimumValue = 0, position = 0) {
   /**
-    @param {array} listOfValues
-    @param {integer} size
-    @return {class} 
-    */
-  constructor(listOfValues, size) {
-    this.listOfValues = listOfValues;
-    this.size = size;
-  }
-
-  findMinimum(minimumValue = 0, position = 0) {
-    /** 
     Exercise 2
     Write a recursive function that finds and returns the minimum element in an array, where the array and its size are given as parameters.
-    
+
     Function returns the minimum value in an array
     Assumption: Only positive integers are provided as size input
-    @params {integer} minimumValue
-    @params {integer} position
+    @param { array } listOfValues
+    @param {integer} size
+    @param {integer} minimumValue
+    @param {integer} position
     @return {integer}
     */
-    if (position === this.size) {
-      return minimumValue;
+  if (position === size) {
+    return minimumValue;
+  } else {
+    if (position === 0) {
+      minimumValue = listOfValues[position];
     } else {
-      if (position === 0) {
-        minimumValue = this.listOfValues[position];
-      } else {
-        this.listOfValues[position] < minimumValue
-          ? (minimumValue = this.listOfValues[position])
-          : minimumValue;
-      }
-      position += 1;
-      return this.findMinimum(minimumValue, position);
+      listOfValues[position] < minimumValue
+        ? (minimumValue = listOfValues[position])
+        : minimumValue;
     }
+    position += 1;
+    return findMinimum(listOfValues, size, minimumValue, position);
   }
+}
 
-  findSumOfArray(total = 0, position = 0) {
-    /** 
+function findSumOfArray(listOfValues, size, total = 0, position = 0) {
+  /**
     Exercise 3
     Write a recursive function that computes and returns the sum of all elements in an array, where the array and its size are given as parameters
-    
+
     Function returns the sum of values in the array
     Assumption: Only positive integers are provided as size input
+    @param { array } listOfValues
+    @param {integer} size
     @param {integer} total
     @param {integer} position
     @return {integer}
     */
-    if (position === this.size) {
-      return total;
-    } else {
-      total = total + this.listOfValues[position];
-      position += 1;
-      return this.findSumOfArray(total, position);
-    }
+  if (position === size) {
+    return total;
+  } else {
+    total = total + listOfValues[position];
+    position += 1;
+    return findSumOfArray(listOfValues, size, total, position);
   }
+}
 
-  findPalindrome(
-    arrayFromFront = "",
-    positionFromFront = 0,
-    arrayFromBack = "",
-    positionFromBack = this.size - 1
-  ) {
-    /** 
+function findPalindrome(
+  listOfValues,
+  size,
+  arrayFromFront = "",
+  positionFromFront = 0,
+  arrayFromBack = "",
+  positionFromBack = size - 1
+) {
+  /**
     Exercise 4
     Write a recursive function that determines whether an array is a palindrome, where the array and its size are given as parameters.
-    
+
     Function returns 1 if the array is a palindromm otherwise 0
     Assumption: Only positive integers are provided as size input
+    @param { array } listOfValues
+    @param {integer} size
     @param {string} arrayFromFront
     @param {integer} positionFromFront
     @param {string} arrayFromBack
     @param {integer} positionFromBack
     @return {integer}
     */
-    if (positionFromFront === this.size) {
-      if (arrayFromFront === arrayFromBack) {
-        return 1;
-      }
-      return 0;
-    } else {
-      arrayFromFront = arrayFromFront + this.listOfValues[positionFromFront];
-      positionFromFront += 1;
-      arrayFromBack = arrayFromBack + this.listOfValues[positionFromBack];
-      positionFromBack -= 1;
-      return this.findPalindrome(
-        arrayFromFront,
-        positionFromFront,
-        arrayFromBack,
-        positionFromBack
-      );
+  if (positionFromFront === size) {
+    console.log(
+      arrayFromBack,
+      arrayFromFront,
+      arrayFromFront === arrayFromBack
+    );
+    if (arrayFromFront === arrayFromBack) {
+      return 1;
     }
+    return 0;
+  } else {
+    arrayFromFront = arrayFromFront + listOfValues[positionFromFront];
+    positionFromFront += 1;
+    arrayFromBack = arrayFromBack + listOfValues[positionFromBack];
+    positionFromBack -= 1;
+    return findPalindrome(
+      listOfValues,
+      size,
+      arrayFromFront,
+      positionFromFront,
+      arrayFromBack,
+      positionFromBack
+    );
   }
 }
 
-module.exports = { PractiseSum, PractiseArrays };
+module.exports = { sumOfNNumbers, findMinimum, findSumOfArray, findPalindrome };
--- test/recurssionTest.js
+++ test/recurssionTest.js
@@ -1,46 +1,40 @@
 "use strict";
 
 const expect = require("chai").expect;
-const { PractiseSum, PractiseArrays } = require("../library/recurssion");
-
-let class1 = new PractiseSum(5);
-let class2 = new PractiseArrays([-1, -10, 11, 5], 4);
-let class3 = new PractiseArrays([-1, -2, -2, -1], 4);
-let class4 = new PractiseArrays([-1, -2, 3, -2, -1], 5);
-let class5 = new PractiseArrays([], 0);
-let class6 = new PractiseArrays(["a", "A", "A"], 3);
+const {
+  sumOfNNumbers,
+  findMinimum,
+  findSumOfArray,
+  findPalindrome
+} = require("../library/recurssion");
 
 describe("#tests for recurssion on integers", function () {
   it("should add first n numbers", function () {
-    expect(class1.sumOfNNumbers()).to.equal(15);
+    expect(sumOfNNumbers(5)).to.equal(15);
   });
 });
 describe("#tests for recurssion for lists", function () {
   it("should find minimum number in array", function () {
-    expect(class2.findMinimum()).to.equal(-10);
+    expect(findMinimum([-1, -10, 11, 5], 4)).to.equal(-10);
   });
 
   it("should find sum of numbers in array", function () {
-    expect(class2.findSumOfArray()).to.equal(5);
+    expect(findSumOfArray([-1, -10, 11, 5], 4)).to.equal(5);
   });
 
   it("should find if array is a palindrome", function () {
-    expect(class2.findPalindrome()).to.equal(0);
+    expect(findPalindrome([-1, -10, 11, 5], 4)).to.equal(0);
   });
 
   it("should find if array is a palindrome", function () {
-    expect(class3.findPalindrome()).to.equal(1);
+    expect(findPalindrome([-1, -2, 3, -2, -1], 5)).to.equal(1);
   });
 
   it("should find if array is a palindrome", function () {
-    expect(class4.findPalindrome()).to.equal(1);
+    expect(findPalindrome([], 0)).to.equal(1);
   });
 
   it("should find if array is a palindrome", function () {
-    expect(class5.findPalindrome()).to.equal(1);
-  });
-
-  it("should find if array is a palindrome", function () {
-    expect(class6.findPalindrome()).to.equal(0);
+    expect(findPalindrome(["a", "A", "A"], 3)).to.equal(0);
   });
 });
